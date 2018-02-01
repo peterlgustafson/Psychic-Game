ba0098bb52fa75a6a1b5d58bf74e54dc1d772a1e
Peter Gustafson <peterlideckergustafson@gmail.com>
2018-02-01 08:07:59 -0600

    minor updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,19 @@
 
 var letters = ["a", "b", "c", "d", "e,", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+//Array for all user guesses
+
+var userGuess = ["Guess 1", "Guess 2", "Guess 3", "Guess 4", "Guess 5", "Guess 6", "Guess 7", "Guess 8", "Guess 9"]
+
 // Creating variables to hold the number of wins and losses. They start at 0.
 
 var wins = 0;
 var losses = 0;
 
-// Variable to count the number of guesses remaining before user takes an L
+// Variable to count the number of total guesses and guesses remaining
 
-var guesses = 9;
+var guesses = 10;
+var guessesLeft = 9;
 
 // Function to Pick Random Letter
 
@@ -37,20 +42,23 @@ document.onkeypress = function (event) {
         guesses+=9;
     }
 
-    // if (guesses === 0) {
-    //     guesses+9;
-    // }
+//Console Log all User Guesses
+console.log(userGuess.toString)
+
+    //Function to re-run picking random letter
+
+// var resetcomputerChoice = letters[Math.floor(Math.random() * letters.length)];
 
 // For loops through the process
 
-    // for (var i = 9; i <= guesses; i--) {
+    // for (var i = 10; i >= guesses.length; i--) {
     //     console.log(guesses[i]);
-    //}
+    // }
     
-//Push to HTML markup
+//Push to innerHTML markup
 
     document.getElementById("wins").innerHTML = "Wins: " + wins;
     document.getElementById("losses").innerHTML = "Losses: " + losses;
-    document.getElementById("guesses").innerHTML = "Guesses left: " + guesses;
+    document.getElementById("guessesLeft").innerHTML = "Guesses left: " + guesses;
     document.getElementById("userGuess").innerHTML = "Your Guesses so far: " + userGuess;
 }
